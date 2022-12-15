6fbf1ead9b495d843b329be8d2129609e49249e6
pixelrunner <19586329+pixelrunner@users.noreply.github.com>
2022-12-15 21:42:34 +0000

    day 7 workings

--- Day7/day7.py
+++ Day7/day7.py
@@ -2,24 +2,60 @@
 #  directory_path: [[file, size],[file, size]]}
 
 
-def main():
+def convertToTuple (itemToConvert):
+    # convert lists of lists inside a dictionary to tuples of tuples inside a dictionary
+    if type(itemToConvert) == dict:
+        for dictKey in itemToConvert.keys():
+            itemToConvert[dictKey] = tuple(tuple(x) for x in itemToConvert[dictKey])
+    return itemToConvert
+
+
+def buildTree():
+    # loop through cli and build dictionary of directories and every file in each of them wth their sizes
     filesAndDirs = {}
     currentPath = ""
     f = open("input.txt", "r")
-    cliLine = f.readline().strip().split()
+    cliLine = f.readline().replace("/n", "").strip().split()
 
-    while cliLine != "":
-        if cliLine[1] == "cd" and cliLine[2] != "..":
-            currentPath = currentPath & cliLine[2]
+    while cliLine != []:
+        if cliLine[1] == "ls":
+            pass
+        elif cliLine[0] == "dir":
+            pass
+        elif cliLine[1] == "cd" and cliLine[2] != "..":
+            currentPath = (currentPath + "/" + cliLine[2]).replace("//", "/")
             filesAndDirs[currentPath] = []
         elif cliLine[0].isalnum():
             filesAndDirs[currentPath].append([cliLine[1], cliLine[0]])
         elif cliLine[1] == "cd" and cliLine[2] == "..":
             currentPath = currentPath.rsplit("/", 1)[0]
 
-        cliLine = f.readline().strip().split()
+        cliLine = f.readline().replace("/n", "").strip().split()
 
-    print(filesAndDirs)
+    return convertToTuple(filesAndDirs)
+
+
+def buildSummary(directoryTree):
+    # take full tree and build a summary dictionary {directory: size}
+    summaryDict = {}
+    for dictKey in directoryTree:
+        cumTotal = 0
+        for file in directoryTree[dictKey]:
+            cumTotal += int(file[1])
+        summaryDict[dictKey] = cumTotal
+    return summaryDict
+
+
+def part1answer(directorySummary):
+    return sum(v for v in directorySummary.values() if v <= 100000)
+
+
+def main():
+    filesAndDirs = buildTree()
+    directorySummary = buildSummary(filesAndDirs)
+
+    print(part1answer(directorySummary))
+
 
 if __name__ == '__main__':
     main()
